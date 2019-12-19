525b447e9d17ae7818f7b40357f5851f8b316907
eels <ely.eels@gmail.com>
2019-12-19 17:35:07 -0500

    updates styling with tachyons

--- .eleventy.js
+++ .eleventy.js
@@ -5,102 +5,102 @@ const fs = require("fs");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster');
- 
+// const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster');
+
 
 module.exports = function(eleventyConfig) {
-  const cacheBusterOptions = {};
-  eleventyConfig.addPlugin(cacheBuster(cacheBusterOptions));
-  // eleventyConfig.addPlugin(pluginRss);
-  // eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.addPlugin(pluginNavigation);
+    // const cacheBusterOptions = {};
+    // eleventyConfig.addPlugin(cacheBuster(cacheBusterOptions));
+    // eleventyConfig.addPlugin(pluginRss);
+    // eleventyConfig.addPlugin(pluginSyntaxHighlight);
+    eleventyConfig.addPlugin(pluginNavigation);
 
-  eleventyConfig.setDataDeepMerge(true);
+    eleventyConfig.setDataDeepMerge(true);
 
-  // eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+    // eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
-  });
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("dd LLL yyyy");
+    });
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-  });
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+    });
 
-  // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
-      return array.slice(n);
+    // Get the first `n` elements of a collection.
+    eleventyConfig.addFilter("head", (array, n) => {
+        if (n < 0) {
+            return array.slice(n);
+        }
+
+        return array.slice(0, n);
+    });
+
+    eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+
+    eleventyConfig.addPassthroughCopy({ "img": "groups/items/img" });
+
+    eleventyConfig.addPassthroughCopy('css')
+    return {
+        passthroughFileCopy: true
     }
 
-    return array.slice(0, n);
-  });
+    /* Markdown Overrides */
+    let markdownLibrary = markdownIt({
+        html: true,
+        breaks: true,
+        linkify: true
+    }).use(markdownItAnchor, {
+        permalink: true,
+        permalinkClass: "direct-link",
+        permalinkSymbol: "#"
+    });
+    eleventyConfig.setLibrary("md", markdownLibrary);
 
-  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+    // Browsersync Overrides
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+            ready: function(err, browserSync) {
+                const content_404 = fs.readFileSync('_site/404.html');
 
-  eleventyConfig.addPassthroughCopy({ "img": "groups/items/img" });
+                browserSync.addMiddleware("*", (req, res) => {
+                    // Provides the 404 content without redirect.
+                    res.write(content_404);
+                    res.end();
+                });
+            }
+        }
+    });
 
-  eleventyConfig.addPassthroughCopy('css')
-  return {
-    passthroughFileCopy: true
-  }
+    return {
+        templateFormats: [
+            "md",
+            "njk",
+            "html",
+            "liquid",
+            "png"
+        ],
 
-  /* Markdown Overrides */
-  let markdownLibrary = markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true
-  }).use(markdownItAnchor, {
-    permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
+        // If your site lives in a different subdirectory, change this.
+        // Leading or trailing slashes are all normalized away, so don’t worry about those.
 
-  // Browsersync Overrides
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('_site/404.html');
+        // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+        // This is only used for link URLs (it does not affect your file structure)
+        // You can also pass this in on the command line using `--pathprefix`
 
-        browserSync.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      }
-    }
-  });
+        // pathPrefix: "/",
 
-  return {
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "liquid",
-      "png"
-    ],
+        markdownTemplateEngine: "liquid",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
 
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about those.
-
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for link URLs (it does not affect your file structure)
-    // You can also pass this in on the command line using `--pathprefix`
-
-    // pathPrefix: "/",
-
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-
-    // These are all optional, defaults are shown:
-    dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
-  };
+        // These are all optional, defaults are shown:
+        dir: {
+            input: ".",
+            includes: "_includes",
+            data: "_data",
+            output: "_site"
+        }
+    };
 };
--- index.md
+++ index.md
@@ -6,7 +6,7 @@ tags:
     - nav
 eleventyNavigation:
   key: home
-  order: 1
+  order: 4
 ---
 
 Construct denim systema cyber-j-pop jeans render-farm rebar plastic shanty town fluidity silent sensory. Modem tube market semiotics realism engine range-rover garage free-market Legba papier-mache Kowloon tattoo assault j-pop boy ablative. Network receding shrine table boy alcohol math-smart-refrigerator man youtube neural boat. Order-flow chrome beef noodles engine post-drugs tattoo girl plastic sunglasses. 
